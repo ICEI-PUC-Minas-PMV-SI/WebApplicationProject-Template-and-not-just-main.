b02e30f48bfac95c5a270b146698082995adec38
Salo Lobato <salo.lobato@gmail.com>
2021-07-04 20:09:44 -0300

    adicionar conexão com a API na página de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,27 +1,11 @@
-function login (username, password) {
-    const accounts = [
-        { "id": 1, "username": "admin", "password": "admin.123", "name": "Administrador", "email": "admin@example.com" },
-        { "id": 2, "username": "sindico", "password": "sindico.123", "name": "Síndico", "email": "sindico@example.com" },
-        { "id": 3, "username": "condomino", "password": "condomino.123", "name": "Condomíno", "email": "condomino@example.com" }
-    ]
-
-    for (var i = 0; i < accounts.length; i++) {
-        var account = accounts[i];
-
-        const currentAccount = {};
-
-        if (username == account.username && password == account.password) {
-           
-            currentAccount.id = account.id;
-            currentAccount.username = account.username;
-            currentAccount.email = account.email;
-            currentAccount.name = account.name;
-            
-            sessionStorage.setItem ('currentAccount', JSON.stringify (currentAccount));
-
-            return true;
-        }
-    }
+async function login(args) {
+  axios.post('http://localhost:3000/auth', args).then(res => {
+    localStorage.setItem('account', JSON.stringify(res.data));
+    
+    return true;
+  }).catch(err => {
+    console.log(err)
 
     return false;
+  })  
 }
