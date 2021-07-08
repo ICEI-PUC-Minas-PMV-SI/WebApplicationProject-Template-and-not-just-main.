e1f642872c91c98a304681b3cc7f2fe06254cc58
Salo Lobato <salo.lobato@gmail.com>
2021-07-08 03:33:42 -0300

    atualização da Página de Login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,25 @@
 async function login(args) {
   axios.post('http://localhost:3000/auth', args).then(res => {
+    //Persiste os dados do usuário no LocalStorage
     localStorage.setItem('account', JSON.stringify(res.data));
-    
-    return true;
-  }).catch(err => {
-    console.log(err)
 
-    return false;
+    //Redireciona para a Página Inicial
+    window.location.href = '../index.html';
+  }).catch(err => {
+    alert('Usuário e/ou senha inválidos!');
   })  
 }
+
+async function handleLogin(event) {                
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const args = {email, password}
+  console.log(args)
+
+  await login(args);
+}
+
+document.getElementById('loginForm').addEventListener('submit', handleLogin);
